d0f556fab731d0658900a0795d08e189a5552c55
Pierre Duchateau <pad@dabla.com>
2023-03-31 19:08:31 +0200

    Eslint+Prettier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     {
       name: '@storybook/addon-postcss',
       options: {
@@ -16,8 +13,8 @@ module.exports = {
       },
     },
   ],
-  "framework": "@storybook/react",
-  "typescript": {
-    "reactDocgen": 'react-docgen-typescript-plugin'
-  }
+  framework: '@storybook/react',
+  typescript: {
+    reactDocgen: 'react-docgen-typescript-plugin',
+  },
 }
